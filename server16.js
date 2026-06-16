8b6611b15685119d8b93c412ca1a6d54727449b8
B Gatherin Blessmi <gatherinblessmi123@gmail.com>
2026-06-16 11:15:09 +0530

    JS

---
+++ server16.js
@@ -0,0 +1,26 @@
+/*console.log("A")
+const promise = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        console.log("Hi")
+    }, 2000)
+    setTimeout(()=>{
+        console.log("Done")
+    },0)
+    console.log("D")
+})
+promise.then(res=>{console.log(res)}).catch(err=>{console.log(err)})*/
+
+console.log(1)
+console.log(2)
+console.log("Hi")
+
+function example(){
+    console.log("Hello")
+}
+const example1=()=>{
+    const a = 1;
+    const b = 2;
+    console.log(1+2)
+}
+setTimeout(example(),3000)
+setTimeout(example1(), 450)
